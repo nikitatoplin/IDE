9d846ac393b32b377d30f117dfdcfe452d79a4e7
Никита Топлин АТ-29 <ntoplin@mail.ru>
2021-11-22 22:08:46 +0500

    файл filter с доктестами

--- filter_with_filename.py
+++ filter_with_filename.py
@@ -1,7 +1,6 @@
 from PIL import Image
 import numpy as np
-import time
-start_time = time.time()
+import doctest
 
 def get_size_and_grad(height_len, width_len):
     correct = True
@@ -14,22 +13,35 @@ def get_size_and_grad(height_len, width_len):
         flag += 1
         array = input().split(' ')
         array = [int(x) for x in array]
-        if height_len % array[0] == 0 and width_len % array[0] == 0 and array[1] < 128:
+        if correct_values(height_len, width_len, array[0], array[1]):
             correct = False
             print('Данные корректы, ищите результат в папке.')
     return(array[0], array[1])
 
 
+def correct_values(hlen, wlen, size, grad):
+    """
+    >>> correct_values(750, 750, 25, 260)
+    False
+    >>> correct_values(750, 750, 31, 15)
+    False
+    """
+    return hlen % size == 0 and wlen % size == 0 and grad < 128
+
+
 def choose_image():
     print('поместите в папку с проектом изображение, которое хотите переименовать, '
-          '/n затем введите название через консоль')
+          'затем введите название через консоль')
     return input()
 
 
-def search_grey(i, j, array, size, grad):
+def search_grey(i, j, array, size):
     sum = int(np.sum(array[i:i + size, j:j + size, 0]) + np.sum(array[i:i + size, j:j + size, 1])
               + np.sum(array[i:i + size, j:j + size, 2])) / 3
-    total_grey = int(sum // (size * size))
+    return int(sum // (size * size))
+
+
+def change_quarter(i, j, array, size, grad, total_grey):
     array[i:i + size, j:j + size, 0] = int(total_grey // grad) * grad
     array[i:i + size, j:j + size, 1] = int(total_grey // grad) * grad
     array[i:i + size, j:j + size, 2] = int(total_grey // grad) * grad
@@ -44,9 +56,10 @@ i = 0
 while i < len_height:
     j = 0
     while j < len_width:
-        search_grey(i, j, pixel_array, size, gradient)
+        total_grey = search_grey(i, j, pixel_array, size)
+        change_quarter(i, j, pixel_array, size, gradient, total_grey)
         j = j + size
     i = i + size
 res = Image.fromarray(pixel_array)
 res.save('res.jpg')
-print("--- %s seconds ---" % (time.time() - start_time))
+doctest.testmod()
